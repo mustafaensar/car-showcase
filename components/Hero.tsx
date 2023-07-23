1956ce59fd89832a69ca127b82a6a96dbb2dffa3
mustafaensar <mustafaensar55@gmail.com>
2023-07-23 23:27:31 +0200

    Navbar and Footer are included

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -23,13 +23,14 @@ const Hero = () => {
           title="Explore Cars"
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
+          btnType="button"
         />
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-          <Image src="/hero.png" alt="hero" fill className="object-contain"/>
-          <div className="hero__image-overlay"></div>
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
         </div>
+        <div className="hero__image-overlay"></div>
       </div>
     </div>
   )
