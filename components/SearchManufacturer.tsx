721232138c3a9ccc3bf6920601593cbcecf5e9af
mustafaensar <mustafaensar55@gmail.com>
2023-09-14 18:12:00 -0500

    test

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState, Fragment } from 'react';
 import { Combobox, Transition } from '@headlessui/react';
-import { SearchManufacturerProps } from '@/types';
+import { SearchManufacturerProps } from '@/types'
 import Image from 'next/image';
 import { manufacturers } from '@/constants';
 
