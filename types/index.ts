83c074ddc62469bae025cd707df2e448458723dd
Mustafa Ensar <mustafaensar55@gmail.com>
2023-10-10 11:24:10 -0500

    customFilter is added

--- types/index.ts
+++ types/index.ts
@@ -37,3 +37,13 @@ export interface FilterProps {
   limit: number;
   model: string;
 }
+
+export interface OptionsProps {
+  title: string;
+  value: string;
+}
+
+export interface CustomFilterProps {
+  title: string;
+  options: OptionsProps[];
+}
